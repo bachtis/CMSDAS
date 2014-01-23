17796f62f03d1a2739977ff0180d65ed7650abac
Michail <bachtis@cern.ch>
2014-01-23 12:20:54 +0100

    finalizing

--- CMSDAS/FourLeptons/python/sources.py
+++ CMSDAS/FourLeptons/python/sources.py
@@ -3,24 +3,24 @@ lumi=19.8
 
 
 
-ZZTo2e2mu = Sample('ZZ',True,['root://eoscms//eos/cms//store/cmst3/user/bachtis/CMSDAS/FourLeptons/ZZTo2e2mu.root'],lumi*1000*0.1767)
+ZZTo2e2mu = Sample('ZZ',True,['root://eoscms//eos/cms//store/cmst3/group/das2014/FourLeptons/ZZTo2e2mu.root'],lumi*1000*0.1767)
 
-ZZTo4e = Sample('ZZ',True,['root://eoscms//eos/cms//store/cmst3/user/bachtis/CMSDAS/FourLeptons/ZZTo4e.root'],lumi*1000*0.07691)
-ZZTo4mu = Sample('ZZ',True,['root://eoscms//eos/cms//store/cmst3/user/bachtis/CMSDAS/FourLeptons/ZZTo4mu.root'],lumi*1000*0.07691)
-GGZZTo2L2L = Sample('ZZ',True,['root://eoscms//eos/cms//store/cmst3/user/bachtis/CMSDAS/FourLeptons/GGZZ2L2L.root'],lumi*1000*0.01203)
-GGZZTo4L = Sample('ZZ',True,['root://eoscms//eos/cms//store/cmst3/user/bachtis/CMSDAS/FourLeptons/GGZZ4L.root'],lumi*1000*0.0048)
-higgs = Sample('higgs',True,['root://eoscms//eos/cms//store/cmst3/user/bachtis/CMSDAS/FourLeptons/higgs.root'],lumi*1000*21.749*3.02e-4)
+ZZTo4e = Sample('ZZ',True,['root://eoscms//eos/cms//store/cmst3/group/das2014/FourLeptons/ZZTo4e.root'],lumi*1000*0.07691)
+ZZTo4mu = Sample('ZZ',True,['root://eoscms//eos/cms//store/cmst3/group/das2014/FourLeptons/ZZTo4mu.root'],lumi*1000*0.07691)
+GGZZTo2L2L = Sample('ZZ',True,['root://eoscms//eos/cms//store/cmst3/group/das2014/FourLeptons/GGZZ2L2L.root'],lumi*1000*0.01203)
+GGZZTo4L = Sample('ZZ',True,['root://eoscms//eos/cms//store/cmst3/group/das2014/FourLeptons/GGZZ4L.root'],lumi*1000*0.0048)
+higgs = Sample('higgs',True,['root://eoscms//eos/cms//store/cmst3/group/das2014/FourLeptons/higgs.root'],lumi*1000*21.749*3.02e-4)
 
 
 
-fakeRate = Sample('fakeRate',False,['root://eoscms//eos/cms//store/cmst3/user/bachtis/CMSDAS/FourLeptons/fakeRate.root'],1)
+fakeRate = Sample('fakeRate',False,['root://eoscms//eos/cms//store/cmst3/group/das2014/FourLeptons/fakeRate.root'],1)
 
 
 
 data_files=[
-'root://eoscms//eos/cms//store/cmst3/user/bachtis/CMSDAS/FourLeptons/DoubleElectron.root',
-'root://eoscms//eos/cms//store/cmst3/user/bachtis/CMSDAS/FourLeptons/DoubleMu.root',
-'root://eoscms//eos/cms//store/cmst3/user/bachtis/CMSDAS/FourLeptons/MuEG.root'
+'root://eoscms//eos/cms//store/cmst3/group/das2014/FourLeptons/DoubleElectron.root',
+'root://eoscms//eos/cms//store/cmst3/group/das2014/FourLeptons/DoubleMu.root',
+'root://eoscms//eos/cms//store/cmst3/group/das2014/FourLeptons/MuEG.root'
 ]
 
 data = Sample('data',False,data_files,1)
